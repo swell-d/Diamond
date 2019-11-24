eed7816e032c28211b04a2ec69d30779c5ea008a
Dmitry Rude <3952800@gmail.com>
2019-11-24 20:25:00 +0100

    second try

--- src/Diamond.java
+++ src/Diamond.java
@@ -9,20 +9,25 @@ class Diamond {
     String draw() {
         String result = "";
         int center = (this.height + 1) / 2;
-        for (int line=1; line <= this.height; line++) {
-            if (line == 1 || line == this.height) {
-                result += repeat(" ", center - 1) + "o" + repeat(" ", center - 1) + "\n";
-            } else {
-                int space_before = Math.abs(center - line);
-                int spaces_between = this.height - space_before * 2 - 2;
-                result += repeat(" ", space_before) +
-                        "o" +
-                        repeat(" ", spaces_between) +
-                        "o" +
-                        repeat(" ", space_before) + "\n";
-            }
-        }
+        for (int line=1; line <= this.height; line++) result += getLine(center, line);
         return result;
+    }
+
+    private String getLine(int center, int line) {
+        if (line == 1 || line == this.height) return getFirstLastLine(center);
+        return getOtherLine(center, line);
+    }
+
+    private String getOtherLine(int center, int line) {
+        int space_before = Math.abs(center - line);
+        int spaces_between = this.height - space_before * 2 - 2;
+        return repeat(" ", space_before) + "o" +
+                repeat(" ", spaces_between) +
+                "o" + repeat(" ", space_before) + "\n";
+    }
+
+    private String getFirstLastLine(int center) {
+        return repeat(" ", center - 1) + "o" + repeat(" ", center - 1) + "\n";
     }
 
     private String repeat(String str, int count){
