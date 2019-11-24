45be3a7b2ce359cd47bcf7126a404e492a4a1f32
Dmitry Rude <3952800@gmail.com>
2019-11-24 21:14:51 +0100

    6th

--- src/Diamond.java
+++ src/Diamond.java
@@ -1,6 +1,7 @@
 class Diamond {
     private int center;
     private int height;
+    private final String blank = " ";
 
     Diamond(int height) {
         if (height % 2 != 1) throw new IllegalArgumentException("Wrong diamond height. Must be positive odd number.");
@@ -25,18 +26,16 @@ class Diamond {
     private String innerLine(int line) {
         int outerSpaces = Math.abs(this.center - line);
         int spacesBetween = this.height - outerSpaces * 2 - 2;
-        return repeat(" ", outerSpaces) + "o" +
-                repeat(" ", spacesBetween) +
-                "o" + repeat(" ", outerSpaces) + "\n";
+        return blanks(outerSpaces) + "o" + blanks(spacesBetween) + "o" + blanks(outerSpaces) + "\n";
     }
 
     private String outerLine() {
-        return repeat(" ", this.center - 1) + "o" + repeat(" ", this.center - 1) + "\n";
+        return blanks(this.center - 1) + "o" + blanks(this.center - 1) + "\n";
     }
 
-    private String repeat(String str, int count){
+    private String blanks(int count){
         String result = "";
-        for (int i=1; i <= count; i++) result += str;
+        for (int i=1; i <= count; i++) result += blank;
         return result;
     }
 }
