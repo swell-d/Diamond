1e5ef5feef80f1f32a379f581e02aad1b6dfe3e8
Dmitry Rude <3952800@gmail.com>
2019-11-24 20:59:36 +0100

    4th try

--- src/Diamond.java
+++ src/Diamond.java
@@ -1,36 +1,37 @@
 class Diamond {
+    private int center;
     private int height;
 
     Diamond(int height) {
         if (height % 2 != 1) throw new IllegalArgumentException("Wrong diamond height. Must be positive odd number.");
         this.height = height;
+        this.center = (this.height + 1) / 2;
     }
 
     String draw() {
         String result = "";
-        int center = (this.height + 1) / 2;
-        for (int line=1; line <= this.height; line++) result += Line(center, line);
+        for (int line=1; line <= this.height; line++) result += Line(line);
         return result;
     }
 
-    private String Line(int center, int line) {
-        return isOuterLine(line)? OuterLine(center) : InnerLine(center, line);
+    private String Line(int line) {
+        return isOuterLine(line)? OuterLine() : InnerLine(line);
     }
 
     private boolean isOuterLine(int line) {
         return line == 1 || line == this.height;
     }
 
-    private String InnerLine(int center, int line) {
-        int spaces_before = Math.abs(center - line);
+    private String InnerLine(int line) {
+        int spaces_before = Math.abs(this.center - line);
         int spaces_between = this.height - spaces_before * 2 - 2;
         return repeat(" ", spaces_before) + "o" +
                 repeat(" ", spaces_between) +
                 "o" + repeat(" ", spaces_before) + "\n";
     }
 
-    private String OuterLine(int center) {
-        return repeat(" ", center - 1) + "o" + repeat(" ", center - 1) + "\n";
+    private String OuterLine() {
+        return repeat(" ", this.center - 1) + "o" + repeat(" ", this.center - 1) + "\n";
     }
 
     private String repeat(String str, int count){
